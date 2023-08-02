d5b356537ac49a310df4d7ac9b1ff4c5e6ab19e7
Anna Petrova <annapetrova@team.amocrm.com>
2023-08-02 18:59:34 +0300

    start work with api client

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,8 +3,143 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use AmoCRM\Client\AmoCRMApiClient;
+use League\OAuth2\Client\Token\AccessTokenInterface;
+use League\OAuth2\Client\Token\AccessToken;
+
+define('TOKEN_FILE', DIRECTORY_SEPARATOR . 'tmp' . DIRECTORY_SEPARATOR . 'token_info.json');
 
 class ContactController extends Controller
 {
-    //
+    private function save_token($accessToken) {
+        if (
+            isset($accessToken)
+            && isset($accessToken['accessToken'])
+            && isset($accessToken['refreshToken'])
+            && isset($accessToken['expires'])
+            && isset($accessToken['baseDomain'])
+        ) {
+            $data = [
+                'accessToken' => $accessToken['accessToken'],
+                'expires' => $accessToken['expires'],
+                'refreshToken' => $accessToken['refreshToken'],
+                'baseDomain' => $accessToken['baseDomain'],
+            ];
+    
+            file_put_contents(TOKEN_FILE, json_encode($data));
+        } else {
+            exit('Invalid access token ' . var_export($accessToken, true));
+        }
+    }
+
+    private function get_token() {
+        if (!file_exists(TOKEN_FILE)) {
+            exit('Access token file not found');
+        }
+    
+        $accessToken = json_decode(file_get_contents(TOKEN_FILE), true);
+    
+        if (
+            isset($accessToken)
+            && isset($accessToken['accessToken'])
+            && isset($accessToken['refreshToken'])
+            && isset($accessToken['expires'])
+            && isset($accessToken['baseDomain'])
+        ) {
+            return new AccessToken([
+                'access_token' => $accessToken['accessToken'],
+                'refresh_token' => $accessToken['refreshToken'],
+                'expires' => $accessToken['expires'],
+                'baseDomain' => $accessToken['baseDomain'],
+            ]);
+        } else {
+            exit('Invalid access token ' . var_export($accessToken, true));
+        }
+    }
+
+    private function token_by_code($apiClient) {
+        session_start();
+
+        if (isset($_GET['referer'])) {
+            $apiClient->setAccountBaseDomain($_GET['referer']);
+        }
+
+        if (!isset($_GET['code'])) {
+            $state = bin2hex(random_bytes(16));
+            $_SESSION['oauth2state'] = $state;
+        if (isset($_GET['button'])) {
+            echo $apiClient->getOAuthClient()->getOAuthButton(
+                [
+                    'title' => 'Установить интеграцию',
+                    'compact' => true,
+                    'class_name' => 'className',
+                    'color' => 'default',
+                    'error_callback' => 'handleOauthError',
+                    'state' => $state,
+                ]
+            );
+            die;
+        } else {
+            $authorizationUrl = $apiClient->getOAuthClient()->getAuthorizeUrl([
+                'state' => $state,
+                'mode' => 'post_message',
+            ]);
+            header('Location: ' . $authorizationUrl);
+            die;
+        }
+        } elseif (!isset($_GET['from_widget']) && (empty($_GET['state']) || empty($_SESSION['oauth2state']) || ($_GET['state'] !== $_SESSION['oauth2state']))) {
+            unset($_SESSION['oauth2state']);
+            exit('Invalid state');
+        }
+
+        try {
+            $accessToken = $apiClient->getOAuthClient()->getAccessTokenByCode($_GET['code']);
+
+            if (!$accessToken->hasExpired()) {
+                $this->save_token([
+                    'accessToken' => $accessToken->getToken(),
+                    'refreshToken' => $accessToken->getRefreshToken(),
+                    'expires' => $accessToken->getExpires(),
+                    'baseDomain' => $apiClient->getAccountBaseDomain(),
+                ]);
+            }
+        } catch (Exception $e) {
+            die((string)$e);
+        }
+
+        return $this->get_token();
+    }
+
+    public function add(Request $request) {
+        $name = $request->input('name');
+        $surname = $request->input('surname');
+        $age = $request->input('age');
+        $sex = $request->input('sex');
+        $phone = $request->input('phone');
+        $email = $request->input('email');
+
+        $clientId = $_ENV['CLIENT_ID'];
+        $clientSecret = $_ENV['CLIENT_SECRET'];
+        $redirectUri = $_ENV['CLIENT_REDIRECT_URI'];
+
+        $apiClient = new AmoCRMApiClient($clientId, $clientSecret, $redirectUri);
+
+        $accessToken = $this->token_by_code($apiClient);
+
+        $apiClient->setAccessToken($accessToken)
+            ->setAccountBaseDomain($accessToken->getValues()['baseDomain'])
+            ->onAccessTokenRefresh(
+                function (AccessTokenInterface $accessToken, string $baseDomain) {
+                    saveToken(
+                        [
+                            'accessToken' => $accessToken->getToken(),
+                            'refreshToken' => $accessToken->getRefreshToken(),
+                            'expires' => $accessToken->getExpires(),
+                            'baseDomain' => $baseDomain,
+                        ]
+                    );
+                }
+            );
+        return $accessToken->getToken();
+    }
 }
